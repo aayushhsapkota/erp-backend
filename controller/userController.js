f374588c43ce68c2fddcb4e181fe92d7c3f75f9d
aayushsapkota1 <kodaayush@gmail.com>
2023-07-01 12:46:14 +0545

    Server side validation for form

--- controller/userController.js
+++ controller/userController.js
@@ -3,19 +3,40 @@ import jwt from "jsonwebtoken";
 
 import userModel from "../models/userModel.js";
 
-const signatureKey="mySecretKey";
+const signatureKey = "mySecretKey";
 
 export const signin = async (req, res) => {
-  const {email, password } = req.body;
+  const { email, password } = req.body;
 
   try {
+    if (!email) {
+      return res.status(400).json({ message: "Please enter email." });
+    }
+
+    if (!password) {
+      return res.status(400).json({ message: "Please enter password." });
+    }
+
     const existingUser = await userModel.findOne({ email });
-    if (!existingUser) return res.status(404).json({ message: "User doesn't exist" });
+    if (!existingUser)
+      return res.status(404).json({ message: "User doesn't exist" });
 
-    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
-    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign({ email: existingUser.email, id: existingUser._id, isAdmin:existingUser.isAdmin }, signatureKey, { expiresIn: "1h" });
+    const token = jwt.sign(
+      {
+        email: existingUser.email,
+        id: existingUser._id,
+        isAdmin: existingUser.isAdmin,
+      },
+      signatureKey,
+      { expiresIn: "1h" }
+    );
 
     res.status(200).json({ result: existingUser, token });
   } catch (err) {
@@ -23,25 +44,54 @@ export const signin = async (req, res) => {
   }
 };
 
-export const signup = async (req, res) => { //this is done by admin.
-  const { email, password,confirmPassword, firstName, lastName } = req.body;
+export const signup = async (req, res) => {
+  //this is done by admin.
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
 
   try {
-    const existingUser = await userModel.findOne({ email });
-    if (existingUser) return res.status(400).json({ message: "User already exists" });
+    if (!email) {
+        return res.status(400).json({ message: "Please enter email." });
+      }
+  
+      if (!password) {
+        return res.status(400).json({ message: "Please enter password." });
+      }
 
-    if (password !== confirmPassword) return res.status(400).json({message:"Passwords don't match"});
+      if (!confirmPassword) {
+        return res.status(400).json({ message: "Please confirm your password." });
+      }
+      if (!firstName ) {
+        return res.status(400).json({ message: "Please enter your first name." });
+      }
+      if (!lastName ) {
+        return res.status(400).json({ message: "Please enter your last name." });
+      }
+
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    if (password !== confirmPassword)
+      return res.status(400).json({ message: "Passwords don't match" });
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const result = await userModel.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` }); 
+    const result = await userModel.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+    });
 
-    const token = jwt.sign( { email: result.email, id: result._id, isAdmin:result.isAdmin }, signatureKey, { expiresIn: "1h" } );
+    const token = jwt.sign(
+      { email: result.email, id: result._id, isAdmin: result.isAdmin },
+      signatureKey,
+      { expiresIn: "1h" }
+    );
 
     res.status(201).json({ result, token });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
-    
+
     console.log(error);
   }
 };
