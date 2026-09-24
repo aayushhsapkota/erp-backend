171f4ef3f8c166d1a4e7f4a973dcec3e27e878dc
Aayush Sapkota <aayushsapkota115@outlook.com>
2026-09-24 10:54:36 +1000

    Refactor express middleware configuration by removing unused body-parser imports and simplifying JSON parsing options

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-// import bodyParser from "body-parser";
 import morgan from "morgan";
 import cors from "cors";
 import ProductRoute from "./routes/productRoute.js";
@@ -18,10 +17,7 @@ import CompanyRoute from "./routes/companyRoute.js";
 const app = express();
 app.use(cors());
 
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));
-// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb" }));
 app.use(morgan("dev"));
 
 app.use("/API/products", ProductRoute);
@@ -37,7 +33,6 @@ app.use("/API/company", CompanyRoute);
 
 // Route();
 const PORT = process.env.PORT || 80;
-mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
